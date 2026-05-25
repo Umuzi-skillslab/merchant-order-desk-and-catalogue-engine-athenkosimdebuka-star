fe5eca1bf77405acbc7a3cb30eb5cf1428d71626
athenkosimdebuka-star <athenkosi.mdebuka@umuzi.org>
2026-05-25 20:36:10 +0200

    Create OrderItem.java

---
+++ OrderItem.java
@@ -0,0 +1,22 @@
+public class OrderItem {
+
+private Product product;
+private int quantity;
+
+public OrderItem(Product product, int quantity) {
+this.product = product;
+this.quantity = quantity;
+}
+
+public double calculateTotal() {
+return product.getPrice() * quantity;
+}
+
+public Product getProduct() {
+return product;
+}
+
+public int getQuantity() {
+return quantity;
+}
+}
